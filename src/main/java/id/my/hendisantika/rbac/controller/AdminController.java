9d10e40de3cc94973225144426982de0222b9b45
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:38:18 +0700

    feat: Add AdminController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbac/controller/AdminController.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.rbac.controller;
+
+import id.my.hendisantika.rbac.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/04/25
+ * Time: 07.37
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/admin")
+@PreAuthorize("hasRole('ADMIN')")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final UserService userService;
+
+}
