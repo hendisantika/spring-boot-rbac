8c962b220326c2a39705236513ab89b4d236f03e
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:32:44 +0700

    feat: Add getUserById in UserService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/service/UserService.java
+++ src/main/java/id/my/hendisantika/rbac/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,5 +31,9 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
     }
 }
