521b15953ef3a3c421ef76943e58490cf720f66f
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:41:18 +0700

    feat: Add getAllOrders in OrderController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/controller/OrderController.java
+++ src/main/java/id/my/hendisantika/rbac/controller/OrderController.java
@@ -1,5 +1,8 @@
 package id.my.hendisantika.rbac.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
+    @GetMapping
+    @PreAuthorize("hasAuthority('order:read')")
+    public ResponseEntity<?> getAllOrders() {
+        // Implementation would go here
+        return ResponseEntity.ok("List of all orders - Requires 'order:read' permission");
+    }
 }
