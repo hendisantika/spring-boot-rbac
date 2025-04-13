9b0bb809e9540504e137995abdd1bf0533a166bc
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:25:43 +0700

    feat: Add RoleService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbac/service/RoleService.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.rbac.service;
+
+import id.my.hendisantika.rbac.repository.PermissionRepository;
+import id.my.hendisantika.rbac.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/04/25
+ * Time: 07.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+    private final PermissionRepository permissionRepository;
+
+}
