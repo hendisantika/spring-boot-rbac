a40d261c8d305b0514672a93e58cc907eb8c042e
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:23:34 +0700

    feat: Add UserRegistrationDto🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbac/dto/UserRegistrationDto.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.rbac.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/04/25
+ * Time: 07.23
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class UserRegistrationDto {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
+    private String password;
+}
