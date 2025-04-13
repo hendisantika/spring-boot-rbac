d18274608dd002486ea5942435b548e4e956da1f
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:32:53 +0700

    feat: Add getUserByUsername in UserService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/service/UserService.java
+++ src/main/java/id/my/hendisantika/rbac/service/UserService.java
@@ -36,4 +36,8 @@ public class UserService {
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
     }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
