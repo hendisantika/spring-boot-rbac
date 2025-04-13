6c0651963a657632093a96959b33f16e447d9556
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:36:28 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbac/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.rbac.config;
+
+import id.my.hendisantika.rbac.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/04/25
+ * Time: 07.35
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+    private final CustomUserDetailsService customUserDetailsService;
+
+}
