358608bdadbc663fa0d11ac8e26044b233a7c0ad
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:40:38 +0700

    feat: Add loginUser in AuthController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/controller/AuthController.java
+++ src/main/java/id/my/hendisantika/rbac/controller/AuthController.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.rbac.controller;
 
+import id.my.hendisantika.rbac.dto.LoginDto;
 import id.my.hendisantika.rbac.dto.UserRegistrationDto;
 import id.my.hendisantika.rbac.model.User;
 import id.my.hendisantika.rbac.service.UserService;
@@ -8,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +43,18 @@ public class AuthController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return ResponseEntity.ok("User logged successfully");
     }
 }
