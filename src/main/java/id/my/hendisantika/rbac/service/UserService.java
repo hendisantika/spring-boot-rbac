fea0db4812c820afad1a395019826cc99eb49fd7
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:32:13 +0700

    feat: Add UserService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbac/service/UserService.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.rbac.service;
+
+import id.my.hendisantika.rbac.repository.RoleRepository;
+import id.my.hendisantika.rbac.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/04/25
+ * Time: 07.31
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+}
