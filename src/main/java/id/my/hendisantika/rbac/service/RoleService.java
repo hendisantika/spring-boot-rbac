c1609f479b294136516dd6ef4ffd2295d78126e4
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:26:21 +0700

    feat: Add getAllRoles in RoleService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/service/RoleService.java
+++ src/main/java/id/my/hendisantika/rbac/service/RoleService.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.rbac.service;
 
+import id.my.hendisantika.rbac.model.Role;
 import id.my.hendisantika.rbac.repository.PermissionRepository;
 import id.my.hendisantika.rbac.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +26,7 @@ public class RoleService {
     private final RoleRepository roleRepository;
     private final PermissionRepository permissionRepository;
 
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
 }
