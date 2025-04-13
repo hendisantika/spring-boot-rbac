9e9e4e2fa78acd5b5ef886e82dfde3320fa218c5
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:38:36 +0700

    feat: Add getAllUsers in AdminController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/controller/AdminController.java
+++ src/main/java/id/my/hendisantika/rbac/controller/AdminController.java
@@ -1,10 +1,15 @@
 package id.my.hendisantika.rbac.controller;
 
+import id.my.hendisantika.rbac.model.User;
 import id.my.hendisantika.rbac.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +30,8 @@ public class AdminController {
 
     private final UserService userService;
 
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
 }
