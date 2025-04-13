19580cad6abf86423a1ed9f49556836c088d35fd
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:36:43 +0700

    feat: Add passwordEncoder in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/rbac/config/SecurityConfig.java
@@ -4,9 +4,12 @@ import id.my.hendisantika.rbac.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +31,8 @@ public class SecurityConfig {
     private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
     private final CustomUserDetailsService customUserDetailsService;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
