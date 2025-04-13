8d47bf92f3b56283ffe3793e70ee425399698dbb
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:41:03 +0700

    feat: Add OrderController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbac/controller/OrderController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.rbac.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/04/25
+ * Time: 07.40
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+}
