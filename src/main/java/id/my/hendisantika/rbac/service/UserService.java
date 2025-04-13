b2584efde453b81226e81f6a30382965428f0f18
hendisantika <hendisantika@yahoo.co.id>
2025-04-13 07:32:34 +0700

    feat: Add getAllUsers in UserService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/rbac/service/UserService.java
+++ src/main/java/id/my/hendisantika/rbac/service/UserService.java
@@ -1,10 +1,13 @@
 package id.my.hendisantika.rbac.service;
 
+import id.my.hendisantika.rbac.model.User;
 import id.my.hendisantika.rbac.repository.RoleRepository;
 import id.my.hendisantika.rbac.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +28,7 @@ public class UserService {
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
